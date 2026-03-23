3be374fd4e7445d28da17f769b91aec7de2a7c1c
Usagi <qltenwkd@naver.com>
2026-03-23 14:03:28 +0900

    객체의 상속성

--- main.cpp
+++ main.cpp
@@ -1,134 +1,60 @@
 #include <iostream>
 using namespace std;
 
-// 생성자와 소멸자
-// 클래스에 소속된 함수들을 멤버 함수라고 함
-// 이 중에서 굉장히 특별한 함수 2종이 있는데, 바로 시작과 끝을 알리는 함수들
-// - 시작(탄생) -> 생성자 (여러개 존재 가능)
-// - 끝 (소멸) -> 소멸자 (오직 1개만)
-// 
-// 
-// [암시적(Implicit) 생성자]
-// 생성자를 명시적으로 만들지 않으면,
-// 아무 인자도 받지 않는 [기본 생성자]가 컴파일러에 의해 자동으로 만들어짐
-// -> 그러나 우리가 명시적(Explicit)으로 아무 생성자 하나 만들면,
-// 자동으로 만들어지던 [기본 생성자]는 더 이상 만들어지지 않음!
-// class는 일종의 설계도
+// 객체지향
+// - 상속성
+// - 은닉성
+// - 다형성
+//
 
-class Knight
+// 메모리
+// [[ Player ]]
+// [ Knight ]
+
+// 상속(Inheritance) ? 부모 -> 자식에게 유산을 물려주는 것
+
+class Player
 {
 public:
-	// [1] 기본 생성자 (인자가 없음)
-	Knight()
-	{
-		cout << "knight() 기본 생성자 호출" << endl;
-		_hp = 100;
-		_attack = 10;
-		_posX = 0;
-		_posY = 0;
+	void Move() { cout << "Player Move 호출" << endl; }
+	void Attack() { cout << "Player Attack 호출" << endl; }
+	void Die() { cout << "Player Die 호출" << endl; }
 
-	}
-
-	// [2] 복사 생성자 (자기 자신의 클래스 참조 타입을 인자로 받음)
-	// (일반적으로 '똑같은' 데이터를 지닌 객체가 생성되길 기대한다)
-	//Knight(const Knight& knight)
-	//{ // 도플갱어 만들떄 편리
-	//	_hp = knight._hp;
-	//	_attack = knight._attack;
-	//	_posX = knight._posX;
-	//	_posY = knight._posY;
-	//}
- 
-//[3] 기타 생성자
-	// 이 중에서 인자를 1개만 받는 [기타 생성자]를
-	// [타입 변환 생성자]라고 부르기도 함
-
-	// 명시적인 용도로만 사용할 것
-	explicit Knight(int hp) // 기본 생성자 아님
-	{
-		cout << "knight(int) 생성자 호출" << endl;
-		_hp = 100;
-		_attack = 10;
-		_posX = 0;
-		_posY = 0;
-	}
-
-	Knight(int hp, int attack, int posX, int posY)
-	{
-		_hp = hp;
-		_attack = attack;
-		_posX = posX;
-		_posY = posY;
-	}
-
-	// 소멸자-main 끝나고 실행됨
-	~Knight()
-	{
-		cout << "~Knight() 기본 소멸자 호출" << endl;
-	}
-
-	// 멤버함수 선언
-	void Move(int y, int x);
-	void Attack();
-	void Die()
-	{
-		_hp = 0;
-		this -> _hp = 1;
-
-		cout << "Die" << endl;
-	}
-
-	// 멤버 변수
+public:
 	int _hp;
 	int _attack;
-	int _posY;
-	int _posX;
+	int _defence;
 };
 
-void Knight::Move(int y, int x)
+class Knight : public Player
 {
-	_posY = y;
-	_posX = x;
-	cout << "Move" << endl;
-}
+public: 
 
-void Knight::Attack()
+public:
+	int _stamina;
+};
+
+class Mage : public Player
 {
-	cout << "Attack : " << _attack << endl;
-}
+public:
 
-void HelloKnight(Knight k)
-{
-	cout << "Hello Knight" << endl;
-}
+public:
+	int _sp;
 
-// Instantiate 객체를 만든다!
+};
+
 int main()
 {
-	Knight k1(100, 10, 0, 0);
+	Knight k;
+
+	k._hp = 100;
+	k._attack = 10;
+	k._defence = 5;
+	k._stamina = 50;
 
 
-	Knight k2(k1);
-	Knight k3 = k1; // 복사 생성자로 만들어짐
-
-	Knight k4;
-	k4 = k1;
-
-	k1.Move(2, 2);
-	k1.Attack();
-	k1.Die();
-
-	// 암시적 형변환 -> 컴파일러가 알아서 바꿔치기
-	int num = 1;
-
-	float f = (float)num; // 명시적 < 우리가 코드로 num을 float 바구니에 넣으라고 주문한것
-	double d = num; // 암시적 << 별 말 안했는데 컴파일러가 알아서 처리하고 있음
-
-	Knight k5;
-	k5 = Knight(1); // 이상한 코드, 타입변환생성자 떄무넹 가능하지만 
-	// k5 = 1; 계속 이렇게 암시적으로 쓰면 에러가 날 수 있음
-
-	HelloKnight(Knight(5));
+	k.Attack();
+	k.Die();
 
 	return 0;
 }
