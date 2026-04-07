d547323930e93258ac5f1583381dc51f2dede6d3
Usagi <qltenwkd@naver.com>
2026-04-07 19:17:31 +0900

    초기화리스트(1)

--- main.cpp
+++ main.cpp
@@ -1,90 +1,67 @@
 #include <iostream>
 using namespace std;
 
-// 초기화 리스트
-// 멤버 변수 초기화 ? 다양한 문법이 존재
-// 귀찮은데 초기화를 왜 해야할까? 
-// - 버그 예방
-// - 포인터 등 주소값이 연루되어 있을 경우
-//
-// 초기화 방법
-// - 생성자 내에서 
-// - 초기화 리스트
-// - C++11 문법
-//
-// 초기화 리스트 
-// - 일단 상속 관계에서 원하는 부모 생성자 호출할 때 필요하다
-// - 생성자 내에서 초기화 vs 초기화 리스트
-// -- 일반 변수는 별 차이 없음
-// -- 멤버 타입이 클래스인 경우 차이가 난다
-// -- 정의함과 동시에 초기화가 필요한 경우 (참조 타입, const 타입)
-//
-
-class Inventory
-{
-public:
-	Inventory() { cout << "Inventory()" << endl; }
-	Inventory(int size) { cout << "Inventory(int size)" << endl; }
-
-	~Inventory() { cout << "~Inventory()" << endl; }
-
-public:
-	int _size = 10;
-};
-
-class Player
-{
-public:
-	Player() {}
-	Player(int id) {}
-
-
-
-public:
-
-};
-
-// Is-A (Knight Is-A Player? 기사는 플레이어다) OK->상속관계
-// Is-A (Knight Is-A Inventory? 기사는 인벤토리인가?) no->포함관계
+// 연산자 오버로딩(Operator Overloading)
 // 
-// Has-A (Knight Has-A Inventory? 기사는 인벤을 갖고 있다) OK->포함관계 
+// 연산자 vs 함수
+// - 연산자는 피연산자의 개수/타입이 고정되어 있음
+// 
+// 연산자 오버로딩?
+// 일단 [연산자 함수]를 정의해야 함
+// 함수로 멤버함수 vs 전역함수가 존재하는 것처럼, 
+// 연산자 함수도 두가지 방식으로 만들 수 있음
 
+// - 멤버 연산자 함수 version
+// -- a op b 형태에서 왼쪽으로 기준으로 실행된 
+// (a가 클래스여야 가능, a를 기준 피연산자 라고 함.
+// - 한계) a가 클래스가 아니면 사용 못함
 
-class Knight : public Player
+class Position
 {
-public:
-	Knight() : Player(1), _hp(100), _inventory(20), _hpRef(_hp), _hpConst(100)
-		/*
-		* 선처리 영역
-		* Inventory()
-		*/
+public: 
+	//RET FUN_NAME(ARG_LIST)
+	Position operator+(const Position& arg)
 	{
-		_hp = 100;
-		//_inventory = Inventory(20); //두번호출두번소멸됨
-		// 초기화를 할떄는 내부에서 하지 말고 시작할때 하기
-
-		//_hpRef = _hp;
-		//_hpConst = 100;
+		Position pos;
+		pos._x = _x + arg._x;
+		pos._y = _y + arg._y;
+		
+		return pos;
+	}
+	
+	Position operator+(int arg)
+	{
+		Position pos;
+		pos._x = _x + arg;
+		pos._y = _y + arg;
+		
+		return pos;
 	}
 
 public:
-	int _hp; // 초기화안하면 쓰레기값이 들어가있음
-	Inventory _inventory;
 
-	int& _hpRef;
-	const int _hpConst;
+	int _x;
+	int _y;
+
 };
 
 int main()
 {
-	Knight k;
+	int a = 1;
+	int b = 2;
 
-	cout << k._hp << endl;
+	Position pos;
+	pos._x = 0;
+	pos._y = 0;
 
-	if (k._hp < 0)
-	{
-		cout << "Knight is Dead" << endl;
-	}
+	Position pos2;
+	pos2._x = 1;
+	pos2._y = 1;
+
+	Position pos3 = pos + pos2;
+	Position pos4 = pos3 + 1;
+
+	pos3 = pos.operator+(pos2);
  
 	return 0;
 }
