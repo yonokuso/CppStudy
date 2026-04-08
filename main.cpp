b685e25f83938b5a12ed10cd46208b96ed9cade7
Usagi <qltenwkd@naver.com>
2026-04-08 17:23:52 +0900

    연산자 오버로딩(1)

--- main.cpp
+++ main.cpp
@@ -16,6 +16,12 @@ using namespace std;
 // (a가 클래스여야 가능, a를 기준 피연산자 라고 함.
 // - 한계) a가 클래스가 아니면 사용 못함
 
+// 대안 - 전역 연산자 함수 version
+// a op b 형태라면 a,b 모두를 연산자 함수의 피연산자로 만들어준다.
+
+// 둘 중 뭐가 더 좋은가? 그런거 없음. 
+// 심지어 둘 중 하나만 지원하는 경우도 있기 때문에
+// 대표적으로 대입 연산자(a=b)는 전역 연산자 version으로는 못 만든다
 class Position
 {
 public: 
@@ -38,12 +44,39 @@ public:
 		return pos;
 	}
 
+	bool operator==(const Position& arg)
+	{
+		return _x == arg._x && _y == arg._y;
+	}
+
+	int operator=(int arg)
+	{
+		_x = arg;
+		_y = arg;
+	}
+
 public:
 
 	int _x;
 	int _y;
 
 };
+
+//void operator=(const Position& a, int b)
+//{
+//	a._x = b;
+//	a._y = b;
+//}
+
+Position operator+(int a, const Position& b)
+{
+	Position ret;
+
+	ret._x = b._x + a;
+	ret._y = b._y + a;
+
+	return ret;
+}
 
 int main()
 {
@@ -60,8 +93,15 @@ int main()
 
 	Position pos3 = pos + pos2;
 	Position pos4 = pos3 + 1;
+	pos3.operator+(1);
 
-	pos3 = pos.operator+(pos2);
+	// 이렇게 커스텀 클래스를 사용해 비교할 일이 종종 생김
+	bool isSame = (pos3 == pos4); // 같다는걸 어떻게 받아들일지?
+
+	Position pos5;
+	pos5 = 5; // 이건 대입
+	
+	//pos3 = pos.operator+(pos2);
  
 	return 0;
 }
